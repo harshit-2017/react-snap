f1304ba53384da93abdde90ebb294bb830c88a21
harshit-2017 <64468082+harshit-2017@users.noreply.github.com>
2022-01-20 18:56:02 +0530

    Update puppeteer_utils.js

--- src/puppeteer_utils.js
+++ src/puppeteer_utils.js
@@ -238,7 +238,7 @@ const crawl = async opt => {
         await page.setUserAgent(options.userAgent);
         const tracker = createTracker(page);
         try {
-          await page.goto(pageUrl, { waitUntil: "networkidle2" });
+          await page.goto(pageUrl, { waitUntil: "load", timeout: 0 });
         } catch (e) {
           e.message = augmentTimeoutError(e.message, tracker);
           throw e;
